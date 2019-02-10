29defd366734b721a91d23238c5a4f933639c898
Andrew Blinov <ablinov@brightgrove.com>
2019-02-10 18:34:47 +0200

    make it better

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'), path = require('path');
+
+const app = express();
+
+app.use()express.static('./dist/apartment-credits-app');
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/apartment-credits-app/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log('server started');
+});
