abd5ba055273b78123347f2b5059070877aa8067
Andrew Blinov <ablinov@brightgrove.com>
2019-02-10 18:42:44 +0200

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express'), path = require('path');
+const express = require('express'),
+    path = require('path');
 
 const app = express();
 
-app.use()express.static('./dist/apartment-credits-app');
+app.use(express.static('./dist/apartment-credits-app'));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/apartment-credits-app/index.html'));
